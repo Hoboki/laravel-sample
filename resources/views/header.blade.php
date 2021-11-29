f619233877b6c79b9d1de6439d377bb66dfbd825
hoboki <tokyo_kiichi_osaka@yahoo.co.jp>
2021-11-29 23:34:42 +0900

    logout button

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,13 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">LARAVEL SAMPLE</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="{{ route('posts.index') }}">タイムライン<span class="sr-only"></span></a>
-      <a class="nav-item nav-link active" href="{{ route('people.index') }}">ユーザー一覧 <span class="sr-only"></span></a>
-      <a class="nav-item nav-link" href="{{ route('people.create') }}">ユーザー作成</a>
+    <a class="navbar-brand" href="#">LARAVEL SAMPLE</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="{{ route('posts.index') }}">タイムライン<span class="sr-only"></span></a>
+            <a class="nav-item nav-link active" href="{{ route('people.index') }}">ユーザー一覧 <span class="sr-only"></span></a>
+            <a class="nav-item nav-link" href="{{ route('people.create') }}">ユーザー作成</a>
+            <a class="nav-item nav-link text-right" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </div>
     </div>
-  </div>
 </nav>
